eb6d5bc7dd6a2c2948261925f1847c6ee45d5c7d
AE86JAY <AE86JAY@users.noreply.github.com>
2021-03-27 16:06:12 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,21 +28,11 @@ sed -i "s/192.168.1.1/192.168.5.1/g" package/base-files/files/bin/config_generat
 # echo '修改 主机名'
 sed -i "s/OpenWrt/ACRH17/g" package/base-files/files/bin/config_generate
 
-# echo '添加apfree-wifidog
-# git clone https://github.com/liudf0716/apfree_wifidog.git ./package/diy/apfree-wifidog
-# echo '添加apfree-wifidog配置界面'
-# git clone https://github.com/AE86JAY/luci-app-apfree_wifidog ./package/diy/luci-apfree-wifidog
-# echo '添加 SSR Plus+'
-git clone https://github.com/Mattraks/helloworld.git ./package/diy/ssrplus
 # echo '添加 小猫咪'
 git clone https://github.com/vernesong/OpenClash.git ./package/diy/OpenClash
 # echo '添加 Passwall'
 git clone https://github.com/xiaorouji/openwrt-passwall.git ./package/diy/passwall
-# echo '添加 HelloWorld'
-# git clone https://github.com/jerrykuku/lua-maxminddb.git ./package/diy/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git ./package/diy/luci-app-vssr
-# echo '添加infinityfreedom主题'
-git clone https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git ./package/lean/luci-theme-infinityfreedom
+# ech
 # echo '调整 SSRP 到 GFW 菜单'
 sed -i 's/services/vpn/g' package/diy/ssrplus/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
 sed -i 's/services/vpn/g' package/diy/ssrplus/luci-app-ssr-plus/luasrc/model/cbi/shadowsocksr/advanced.lua
@@ -69,18 +59,6 @@ sed -i 's/services/vpn/g' package/diy/ssrplus/luci-app-ssr-plus/luasrc/view/shad
 # echo '调整 PassWall 到 GFW 菜单'
 sed -i 's/services/vpn/g' package/diy/passwall/luci-app-passwall/luasrc/controller/passwall.lua
 sed -i 's/services/vpn/g' package/diy/passwall/luci-app-passwall/luasrc/model/cbi/passwall/api/api.lua
-# echo '调整 HelloWorld 到 GFW 菜单'
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/controller/vssr.lua
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/model/cbi/vssr/client-config.lua
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/model/cbi/vssr/server.lua
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/model/cbi/vssr/server-config.lua
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/model/cbi/vssr/servers.lua
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/model/cbi/vssr/subscribe-config.lua
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/view/vssr/refresh.htm
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/view/vssr/ssrurl.htm
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/view/vssr/status_top.htm
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/view/vssr/tblsection.htm
-sed -i 's/services/vpn/g' package/diy/luci-app-vssr/luasrc/view/vssr/update_subscribe.htm
 # echo '调整 OpenClash 到 GFW 菜单'
 sed -i 's/services/vpn/g' package/diy/OpenClash/luci-app-openclash/luasrc/controller/openclash.lua
 sed -i 's/services/vpn/g' package/diy/OpenClash/luci-app-openclash/luasrc/model/cbi/openclash/client.lua
